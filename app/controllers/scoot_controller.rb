39c8a78900b9042a5d1840f3e07527a144ad7c02
Ian Golden <ian@iangolden.com>
2015-07-04 15:44:50 -0600

    Bower RC and Bower JSON crudely setup
      * Rendering the templates
      * No functionality in bowerrc, just directory
      * bower.json needs bootstrap

--- app/controllers/scoot_controller.rb
+++ app/controllers/scoot_controller.rb
@@ -19,19 +19,15 @@ class ScootController < ApplicationController
          assets = "#{Rails.root.to_s}/app/views/scoot/assets"
          @assets_link = assets
 
-         @framework
-         @compiler
-         @asset_pipeline
+         @framework = params[:framework]
+         @compiler = params[:compiler]
+         @asset_pipeline = params[:asset_pipeline]
 
-           if params[:framework] == "bootstrap"
-           end
-           if params[:framework] == "foundation"
-           end
-           if params[:preprocessor] == "sass"
-           end
-
-
-         def setup_gemfile
+         def setup_folders
+           FileUtils.mkdir("#{@dir.to_s}/_assets") 
+           FileUtils.mkdir("#{@dir.to_s}/_includes") 
+           FileUtils.mkdir("#{@dir.to_s}/_layouts") 
+           FileUtils.mkdir("#{@dir.to_s}/_plugins") 
          end
 
          def setup_assets
@@ -41,11 +37,13 @@ class ScootController < ApplicationController
          end
 
          def render_files
+           if @asset_pipeline == 'yes'
            bowerrc = render_to_string( :partial => 'bowerrc', :formats => [:html] )
-           gemfile = render_to_string( :partial => 'bowerrc', :formats => [:html] )
-           bowerjson = render_to_string( :partial => 'bowerrc', :formats => [:html] )
-
            File.open("#{@dir.to_s}/.bowerrc", "w+"){|f| f << bowerrc }
+           end
+
+           bowerjson = render_to_string( :partial => 'bowerjson', :formats => [:html] )
+           gemfile = render_to_string( :partial => 'gemfile', :formats => [:html] )
            File.open("#{@dir.to_s}/bower.json", "w+"){|f| f << bowerjson }
            File.open("#{@dir.to_s}/Gemfile", "w+"){|f| f << gemfile }
 
@@ -63,6 +61,7 @@ class ScootController < ApplicationController
            export_file.send
          end
 
+         setup_folders
          render_files
          export_zip_files
 
